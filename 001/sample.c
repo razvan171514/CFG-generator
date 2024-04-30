9489196818558cbf63a11ba8fc25d85879b8d3cd
Razvan Bolundut <razvan.bolundut@yahoo.com>
2024-04-30 09:32:33 +0300

    1st example

---
+++ 001/sample.c
@@ -0,0 +1,1575 @@
+#pragma clang diagnostic ignored "-Wunused-value"
+#pragma clang diagnostic ignored "-Wliteral-range"
+
+int setsockopt_test(int a, int b);
+int ns_capable(int cap);
+int ratio(float ambience, short incision, float dissonance);
+double rag(long damaged, char preparation, char nationality, float jewel, long answer, int division);
+short trading(double understanding, short curtain);
+double forearm(long creepy, float infinite, float aggression, float powder, long ingrate);
+double bathtub(long highfalutin, short metallurgist, long gosling, double broiler);
+short joke(char wise, float evaluator, short nymph);
+char mango(int developer, float diagram, int floozie);
+long paperwork(char share);
+float survey(int bankbook, double policy, long courthouse, char transportation, double pit);
+int instrumentation(double engineer, long erratic, float guide, char spring, char gutter, char imminent);
+
+int setsockopt_test(int a, int b)
+{
+	short var_1  = -22974;
+	long var_2  = -1031650193;
+	short var_3  = 20073;
+	char var_4  = 'p';
+	char var_5  = 'X';
+	
+	a;
+	if (a == 94)
+	{
+		var_5;
+		var_2;
+		if (var_2 < 1979884881)
+		{
+			var_2;
+			var_4;
+			var_1;
+			if (b >= 2)
+			{
+				var_4;
+				b;
+				var_5;
+				var_1;
+			}
+			else
+			{
+				a;
+				b;
+			}
+			var_2;
+			a;
+			var_2;
+			b;
+			ratio(88.27963769030583, var_3, 18.20379832344034);
+		}
+		else
+		{
+			var_2;
+			var_4;
+			b;
+			if (var_4 <= 'W')
+			{
+				b;
+				var_3;
+			}
+			else
+			{
+				b;
+			}
+			var_5;
+			var_3;
+			var_2;
+			var_3;
+			var_2;
+			rag(var_2, var_5, var_4, 47.66305226219302, 1557530184, b);
+		}
+		var_4;
+		var_5;
+		a;
+		var_4;
+		
+	}
+	else
+	{
+		var_2;
+		a;
+		a;
+		if (var_2 <= 2019081571)
+		{
+			var_1;
+			b;
+			var_1;
+			var_1;
+			if (var_2 <= -1782364357)
+			{
+				var_1;
+				var_3;
+				a;
+				a;
+				a;
+			}
+			else
+			{
+				var_5;
+				var_3;
+				b;
+			}
+			var_2;
+			var_5;
+			
+		}
+		else
+		{
+			var_2;
+			a;
+			a;
+			a;
+			var_1;
+			if (var_3 > -18700)
+			{
+				var_5;
+				var_4;
+				a;
+				b;
+			}
+			else
+			{
+				var_3;
+				var_3;
+				var_4;
+				var_4;
+			}
+			var_2;
+			
+		}
+		var_1;
+		
+	}
+	var_4;
+	var_1;
+	var_2;
+	var_2;
+	var_3;
+	
+	
+	
+	return a;
+}
+
+int ns_capable(int cap)
+{
+	int var_1  = -28;
+	float var_2  = 30.241095641840843;
+	double var_3  = -377.98902945145005;
+	float var_4  = -63.17569731301016;
+	double var_5  = 379.4818848211344;
+	
+	var_1;
+	if (var_3 > -672.3062892772411)
+	{
+		var_1;
+		var_5;
+		if (cap == 34)
+		{
+			var_4;
+			var_1;
+			cap;
+			if (var_3 != 561.3050570562314)
+			{
+				var_1;
+				var_3;
+				var_2;
+				var_4;
+			}
+			else
+			{
+				var_5;
+				var_1;
+			}
+			var_3;
+			var_4;
+			var_3;
+			var_2;
+			
+		}
+		else
+		{
+			cap;
+			cap;
+			var_2;
+			if (var_1 == -19)
+			{
+				var_5;
+				var_5;
+			}
+			else
+			{
+				var_1;
+			}
+			var_3;
+			cap;
+			var_4;
+			var_1;
+			var_2;
+			
+		}
+		var_1;
+		var_5;
+		var_3;
+		var_3;
+		
+	}
+	else
+	{
+		var_5;
+		var_4;
+		var_3;
+		if (var_1 < -36)
+		{
+			var_5;
+			var_2;
+			var_3;
+			cap;
+			if (var_5 >= 369.98795746625)
+			{
+				var_4;
+				cap;
+				var_2;
+				var_3;
+				var_1;
+			}
+			else
+			{
+				var_1;
+				var_1;
+				var_2;
+			}
+			var_5;
+			var_5;
+			
+		}
+		else
+		{
+			var_1;
+			var_5;
+			var_3;
+			var_2;
+			var_2;
+			if (var_3 == 486.74209901658105)
+			{
+				var_4;
+				var_1;
+				var_4;
+				var_2;
+			}
+			else
+			{
+				var_4;
+				cap;
+				var_1;
+				var_2;
+			}
+			var_3;
+			
+		}
+		var_3;
+		
+	}
+	var_4;
+	var_1;
+	var_4;
+	var_1;
+	var_2;
+	
+	
+	
+	return cap;
+}
+
+int ratio(float ambience, short incision, float dissonance)
+{
+	short var_1  = -23634;
+	int var_2  = -25;
+	char var_3  = 'x';
+	float var_4  = 49.23311049378782;
+	int var_5  = 60;
+	
+	var_2;
+	if (var_3 > 'l')
+	{
+		incision;
+		dissonance;
+		if (var_2 < 2)
+		{
+			var_1;
+			var_2;
+			dissonance;
+			if (var_3 != 'E')
+			{
+				var_4;
+				incision;
+				dissonance;
+				var_5;
+			}
+			else
+			{
+				var_3;
+				var_5;
+			}
+			var_1;
+			var_5;
+			ambience;
+			incision;
+			rag(1740784596, var_3, 'A', var_4, 870799462, var_5);
+		}
+		else
+		{
+			var_4;
+			ambience;
+			var_3;
+			if (incision <= 20131)
+			{
+				incision;
+				var_5;
+			}
+			else
+			{
+				var_1;
+			}
+			var_5;
+			var_2;
+			var_3;
+			var_3;
+			var_5;
+			ns_capable(var_5);
+		}
+		dissonance;
+		dissonance;
+		var_1;
+		var_3;
+		
+	}
+	else
+	{
+		incision;
+		incision;
+		var_1;
+		if (dissonance < -39.7432030879247)
+		{
+			var_4;
+			dissonance;
+			incision;
+			var_4;
+			if (var_3 != 'k')
+			{
+				var_2;
+				var_2;
+				ambience;
+				var_4;
+				incision;
+			}
+			else
+			{
+				incision;
+				ambience;
+				var_3;
+			}
+			var_1;
+			dissonance;
+			
+		}
+		else
+		{
+			dissonance;
+			var_1;
+			var_2;
+			ambience;
+			var_5;
+			if (ambience >= 78.92760116728826)
+			{
+				incision;
+				var_3;
+				var_2;
+				var_4;
+			}
+			else
+			{
+				incision;
+				var_2;
+				var_1;
+				dissonance;
+			}
+			var_5;
+			
+		}
+		var_5;
+		
+	}
+	ambience;
+	var_3;
+	ambience;
+	var_1;
+	var_4;
+	
+	
+	
+	return var_5;
+}
+
+double rag(long damaged, char preparation, char nationality, float jewel, long answer, int division)
+{
+	double var_1  = -504.180663245706;
+	char var_2  = 'F';
+	float var_3  = -80.81171648315734;
+	short var_4  = -16211;
+	float var_5  = -64.84108167737153;
+	
+	var_5;
+	if (damaged > -1606330326)
+	{
+		nationality;
+		preparation;
+		if (var_5 < -38.17478099306422)
+		{
+			nationality;
+			var_3;
+			answer;
+			if (var_4 == 23320)
+			{
+				var_4;
+				var_4;
+				var_3;
+				var_3;
+			}
+			else
+			{
+				jewel;
+				var_4;
+			}
+			damaged;
+			jewel;
+			var_5;
+			var_5;
+			trading(var_1, var_4);
+		}
+		else
+		{
+			damaged;
+			var_3;
+			division;
+			if (nationality >= 'f')
+			{
+				division;
+				answer;
+			}
+			else
+			{
+				damaged;
+			}
+			nationality;
+			preparation;
+			var_2;
+			var_1;
+			answer;
+			ns_capable(division);
+		}
+		damaged;
+		damaged;
+		var_4;
+		var_1;
+		forearm(answer, jewel, var_5, var_3, damaged);
+	}
+	else
+	{
+		jewel;
+		answer;
+		var_3;
+		if (var_3 < -47.97612587159967)
+		{
+			preparation;
+			jewel;
+			answer;
+			var_3;
+			if (var_2 >= 'g')
+			{
+				var_4;
+				division;
+				var_3;
+				answer;
+				preparation;
+			}
+			else
+			{
+				nationality;
+				var_4;
+				preparation;
+			}
+			answer;
+			var_4;
+			instrumentation(var_1, answer, jewel, var_2, nationality, preparation);
+		}
+		else
+		{
+			preparation;
+			var_3;
+			damaged;
+			var_4;
+			preparation;
+			if (var_3 > -61.7566933495397)
+			{
+				answer;
+				division;
+				var_5;
+				damaged;
+			}
+			else
+			{
+				var_1;
+				var_3;
+				var_3;
+				preparation;
+			}
+			var_4;
+			
+		}
+		var_2;
+		
+	}
+	damaged;
+	nationality;
+	var_3;
+	jewel;
+	answer;
+	
+	
+	
+	return var_1;
+}
+
+short trading(double understanding, short curtain)
+{
+	int var_1  = 77;
+	long var_2  = -2093913769;
+	double var_3  = 777.3066096343109;
+	char var_4  = 'f';
+	float var_5  = -8.140466108647999;
+	
+	understanding;
+	if (understanding != 962.6549111098182)
+	{
+		var_3;
+		curtain;
+		if (var_3 == 204.55962236444452)
+		{
+			understanding;
+			var_5;
+			var_1;
+			if (curtain < 12582)
+			{
+				understanding;
+				var_3;
+				var_1;
+				var_3;
+			}
+			else
+			{
+				var_2;
+				var_3;
+			}
+			var_2;
+			var_5;
+			var_2;
+			var_2;
+			forearm(var_2, var_5, 82.29161540441109, -93.35241802437086, 22822587);
+		}
+		else
+		{
+			var_1;
+			var_1;
+			var_2;
+			if (curtain > 25669)
+			{
+				var_5;
+				var_2;
+			}
+			else
+			{
+				var_2;
+			}
+			var_5;
+			var_3;
+			var_5;
+			var_3;
+			var_3;
+			instrumentation(var_3, var_2, var_5, var_4, 'S', 'P');
+		}
+		understanding;
+		var_3;
+		var_2;
+		var_4;
+		
+	}
+	else
+	{
+		var_3;
+		var_3;
+		var_2;
+		if (var_2 > -56385834)
+		{
+			var_2;
+			var_2;
+			var_3;
+			var_4;
+			if (curtain == 28011)
+			{
+				var_5;
+				var_1;
+				var_5;
+				var_4;
+				var_5;
+			}
+			else
+			{
+				var_2;
+				var_5;
+				var_2;
+			}
+			var_3;
+			understanding;
+			
+		}
+		else
+		{
+			understanding;
+			curtain;
+			curtain;
+			var_5;
+			understanding;
+			if (var_1 <= 29)
+			{
+				var_1;
+				var_3;
+				curtain;
+				var_3;
+			}
+			else
+			{
+				var_2;
+				var_5;
+				var_5;
+				var_5;
+			}
+			var_4;
+			
+		}
+		curtain;
+		
+	}
+	var_1;
+	curtain;
+	var_4;
+	var_1;
+	understanding;
+	
+	
+	
+	return curtain;
+}
+
+double forearm(long creepy, float infinite, float aggression, float powder, long ingrate)
+{
+	long var_1  = -1460627710;
+	double var_2  = 952.3667444602929;
+	float var_3  = 6.034068458412946;
+	float var_4  = 6.62459007072529;
+	int var_5  = -47;
+	
+	var_5;
+	if (var_1 == -1513147712)
+	{
+		var_5;
+		var_1;
+		if (var_3 != 75.36179444145122)
+		{
+			infinite;
+			var_1;
+			powder;
+			if (var_2 >= 449.7523305276268)
+			{
+				infinite;
+				infinite;
+				powder;
+				infinite;
+			}
+			else
+			{
+				var_5;
+				var_5;
+			}
+			var_5;
+			ingrate;
+			ingrate;
+			ingrate;
+			bathtub(var_1, -27525, creepy, var_2);
+		}
+		else
+		{
+			infinite;
+			var_5;
+			powder;
+			if (var_2 > 344.9686840318345)
+			{
+				var_5;
+				var_3;
+			}
+			else
+			{
+				ingrate;
+			}
+			var_3;
+			var_5;
+			creepy;
+			infinite;
+			powder;
+			joke('G', aggression, 22350);
+		}
+		var_1;
+		ingrate;
+		creepy;
+		infinite;
+		
+	}
+	else
+	{
+		powder;
+		var_4;
+		ingrate;
+		if (var_5 <= -77)
+		{
+			ingrate;
+			ingrate;
+			aggression;
+			infinite;
+			if (var_1 == -1624003984)
+			{
+				var_2;
+				var_5;
+				var_1;
+				aggression;
+				aggression;
+			}
+			else
+			{
+				aggression;
+				var_5;
+				powder;
+			}
+			powder;
+			var_3;
+			
+		}
+		else
+		{
+			creepy;
+			powder;
+			var_4;
+			var_4;
+			var_5;
+			if (var_5 >= 1)
+			{
+				var_2;
+				var_5;
+				var_2;
+				aggression;
+			}
+			else
+			{
+				powder;
+				powder;
+				var_1;
+				ingrate;
+			}
+			powder;
+			
+		}
+		creepy;
+		
+	}
+	var_1;
+	ingrate;
+	creepy;
+	var_3;
+	var_3;
+	
+	
+	
+	return var_2;
+}
+
+double bathtub(long highfalutin, short metallurgist, long gosling, double broiler)
+{
+	long var_1  = -251493456;
+	double var_2  = -696.9870043459367;
+	char var_3  = 'r';
+	char var_4  = 'U';
+	long var_5  = 579216768;
+	
+	broiler;
+	if (var_4 > 'U')
+	{
+		gosling;
+		highfalutin;
+		if (var_4 > 'v')
+		{
+			gosling;
+			var_5;
+			var_4;
+			if (var_5 < -1176604089)
+			{
+				var_3;
+				var_3;
+				var_4;
+				broiler;
+			}
+			else
+			{
+				var_3;
+				var_5;
+			}
+			highfalutin;
+			var_2;
+			broiler;
+			metallurgist;
+			joke(var_3, -63.12606017782194, metallurgist);
+		}
+		else
+		{
+			var_1;
+			var_3;
+			var_4;
+			if (gosling == -1064083395)
+			{
+				metallurgist;
+				var_2;
+			}
+			else
+			{
+				gosling;
+			}
+			broiler;
+			var_4;
+			highfalutin;
+			var_3;
+			highfalutin;
+			ns_capable(-73);
+		}
+		var_5;
+		var_4;
+		var_2;
+		var_4;
+		
+	}
+	else
+	{
+		var_1;
+		var_1;
+		var_2;
+		if (var_2 > 113.55206298670828)
+		{
+			var_5;
+			broiler;
+			gosling;
+			highfalutin;
+			if (var_3 <= 'p')
+			{
+				highfalutin;
+				metallurgist;
+				highfalutin;
+				var_2;
+				var_5;
+			}
+			else
+			{
+				highfalutin;
+				broiler;
+				metallurgist;
+			}
+			metallurgist;
+			var_1;
+			
+		}
+		else
+		{
+			metallurgist;
+			metallurgist;
+			var_4;
+			var_2;
+			var_1;
+			if (var_3 >= 'A')
+			{
+				var_2;
+				broiler;
+				gosling;
+				var_2;
+			}
+			else
+			{
+				var_2;
+				var_4;
+				highfalutin;
+				var_4;
+			}
+			var_2;
+			
+		}
+		var_1;
+		
+	}
+	var_3;
+	metallurgist;
+	var_2;
+	broiler;
+	var_1;
+	
+	
+	
+	return broiler;
+}
+
+short joke(char wise, float evaluator, short nymph)
+{
+	float var_1  = 29.666712792331367;
+	char var_2  = 'F';
+	short var_3  = 8574;
+	char var_4  = 'E';
+	long var_5  = -1533325142;
+	
+	var_4;
+	if (var_4 == 'k')
+	{
+		var_2;
+		var_1;
+		if (var_3 == 16315)
+		{
+			var_4;
+			var_1;
+			wise;
+			if (evaluator != 67.66170585431615)
+			{
+				nymph;
+				wise;
+				wise;
+				nymph;
+			}
+			else
+			{
+				var_5;
+				var_2;
+			}
+			wise;
+			var_4;
+			var_3;
+			evaluator;
+			mango(24, evaluator, 63);
+		}
+		else
+		{
+			wise;
+			nymph;
+			evaluator;
+			if (var_2 <= 'I')
+			{
+				var_2;
+				evaluator;
+			}
+			else
+			{
+				var_5;
+			}
+			var_4;
+			nymph;
+			var_1;
+			nymph;
+			var_5;
+			
+		}
+		nymph;
+		var_5;
+		var_2;
+		var_3;
+		
+	}
+	else
+	{
+		var_1;
+		evaluator;
+		nymph;
+		if (var_2 >= 'W')
+		{
+			var_3;
+			evaluator;
+			wise;
+			var_3;
+			if (evaluator < 54.830702208969115)
+			{
+				var_4;
+				var_3;
+				nymph;
+				var_1;
+				evaluator;
+			}
+			else
+			{
+				evaluator;
+				var_2;
+				var_2;
+			}
+			var_4;
+			evaluator;
+			
+		}
+		else
+		{
+			nymph;
+			var_1;
+			var_3;
+			wise;
+			var_1;
+			if (nymph < 23708)
+			{
+				var_4;
+				wise;
+				var_5;
+				var_3;
+			}
+			else
+			{
+				var_2;
+				evaluator;
+				nymph;
+				nymph;
+			}
+			wise;
+			
+		}
+		wise;
+		
+	}
+	var_1;
+	evaluator;
+	evaluator;
+	var_5;
+	var_1;
+	
+	
+	
+	return nymph;
+}
+
+char mango(int developer, float diagram, int floozie)
+{
+	short var_1  = 6624;
+	double var_2  = 203.95300299191354;
+	double var_3  = 924.1146806183488;
+	short var_4  = 101;
+	long var_5  = 397989326;
+	
+	floozie;
+	if (developer != 43)
+	{
+		developer;
+		var_4;
+		if (var_5 != -1243875412)
+		{
+			var_3;
+			var_3;
+			var_1;
+			if (diagram < 82.66098264873699)
+			{
+				var_2;
+				var_5;
+				var_5;
+				var_1;
+			}
+			else
+			{
+				var_5;
+				floozie;
+			}
+			diagram;
+			var_1;
+			developer;
+			floozie;
+			paperwork('k');
+		}
+		else
+		{
+			diagram;
+			var_1;
+			var_4;
+			if (developer == 43)
+			{
+				floozie;
+				var_4;
+			}
+			else
+			{
+				var_2;
+			}
+			var_5;
+			floozie;
+			var_5;
+			diagram;
+			var_4;
+			
+		}
+		var_5;
+		var_3;
+		developer;
+		var_1;
+		
+	}
+	else
+	{
+		var_2;
+		var_4;
+		diagram;
+		if (diagram >= 77.69799777359921)
+		{
+			var_2;
+			floozie;
+			floozie;
+			var_4;
+			if (var_5 == 624988280)
+			{
+				var_3;
+				var_5;
+				var_2;
+				var_3;
+				floozie;
+			}
+			else
+			{
+				var_2;
+				var_4;
+				developer;
+			}
+			var_2;
+			var_3;
+			
+		}
+		else
+		{
+			developer;
+			floozie;
+			var_1;
+			var_4;
+			var_2;
+			if (var_4 > -3238)
+			{
+				diagram;
+				diagram;
+				var_4;
+				var_4;
+			}
+			else
+			{
+				floozie;
+				developer;
+				var_2;
+				var_5;
+			}
+			diagram;
+			
+		}
+		var_1;
+		
+	}
+	floozie;
+	var_5;
+	var_3;
+	var_3;
+	var_1;
+	
+	
+	
+	return 'f';
+}
+
+long paperwork(char share)
+{
+	char var_1  = 'F';
+	int var_2  = -30;
+	char var_3  = 'U';
+	long var_4  = -197644414;
+	float var_5  = 36.689687250071586;
+	
+	var_3;
+	if (var_3 == 'K')
+	{
+		var_4;
+		var_3;
+		if (var_4 <= 1063544699)
+		{
+			var_5;
+			share;
+			var_4;
+			if (var_2 <= -47)
+			{
+				share;
+				var_5;
+				share;
+				var_3;
+			}
+			else
+			{
+				var_1;
+				share;
+			}
+			var_1;
+			var_4;
+			var_3;
+			var_1;
+			survey(var_2, 739.1226110491687, var_4, share, -606.2280112460638);
+		}
+		else
+		{
+			var_3;
+			var_5;
+			var_5;
+			if (var_5 < -51.452338656436545)
+			{
+				var_1;
+				var_2;
+			}
+			else
+			{
+				var_1;
+			}
+			var_1;
+			var_5;
+			var_2;
+			var_5;
+			var_2;
+			
+		}
+		var_5;
+		var_4;
+		var_2;
+		var_4;
+		
+	}
+	else
+	{
+		var_3;
+		var_4;
+		var_4;
+		if (var_1 >= 's')
+		{
+			share;
+			var_1;
+			var_5;
+			var_4;
+			if (var_4 <= 1772635322)
+			{
+				var_4;
+				var_4;
+				var_4;
+				var_1;
+				var_2;
+			}
+			else
+			{
+				var_2;
+				var_5;
+				var_1;
+			}
+			var_2;
+			var_4;
+			
+		}
+		else
+		{
+			var_2;
+			share;
+			var_1;
+			var_5;
+			var_1;
+			if (var_3 >= 'a')
+			{
+				var_4;
+				var_3;
+				var_2;
+				var_3;
+			}
+			else
+			{
+				var_3;
+				var_1;
+				var_5;
+				var_2;
+			}
+			share;
+			
+		}
+		share;
+		
+	}
+	var_3;
+	share;
+	var_1;
+	var_1;
+	var_2;
+	
+	
+	
+	return var_4;
+}
+
+float survey(int bankbook, double policy, long courthouse, char transportation, double pit)
+{
+	long var_1  = -1723632847;
+	char var_2  = 'f';
+	short var_3  = -14938;
+	double var_4  = -629.4950993104926;
+	char var_5  = 't';
+	
+	var_4;
+	if (courthouse >= 1089067602)
+	{
+		pit;
+		pit;
+		if (var_3 == 9163)
+		{
+			policy;
+			transportation;
+			var_4;
+			if (var_4 != 852.4368969152767)
+			{
+				policy;
+				var_2;
+				var_4;
+				bankbook;
+			}
+			else
+			{
+				transportation;
+				var_2;
+			}
+			courthouse;
+			var_2;
+			var_3;
+			courthouse;
+			instrumentation(pit, courthouse, -89.48344951341609, transportation, var_5, var_2);
+		}
+		else
+		{
+			var_4;
+			bankbook;
+			var_5;
+			if (transportation != 'v')
+			{
+				var_5;
+				var_3;
+			}
+			else
+			{
+				var_4;
+			}
+			courthouse;
+			policy;
+			bankbook;
+			var_1;
+			bankbook;
+			ns_capable(bankbook);
+		}
+		var_1;
+		policy;
+		policy;
+		transportation;
+		
+	}
+	else
+	{
+		policy;
+		policy;
+		policy;
+		if (pit <= 320.93652802795395)
+		{
+			var_3;
+			transportation;
+			bankbook;
+			var_3;
+			if (policy > -200.595109695058)
+			{
+				var_2;
+				var_3;
+				policy;
+				var_2;
+				var_5;
+			}
+			else
+			{
+				var_2;
+				bankbook;
+				var_1;
+			}
+			courthouse;
+			var_1;
+			
+		}
+		else
+		{
+			var_1;
+			var_3;
+			pit;
+			courthouse;
+			var_4;
+			if (var_4 == 699.0855933463938)
+			{
+				var_3;
+				pit;
+				policy;
+				var_3;
+			}
+			else
+			{
+				transportation;
+				var_1;
+				bankbook;
+				var_2;
+			}
+			transportation;
+			
+		}
+		pit;
+		
+	}
+	var_5;
+	var_1;
+	courthouse;
+	var_4;
+	courthouse;
+	
+	
+	
+	return 62.76312235974777;
+}
+
+int instrumentation(double engineer, long erratic, float guide, char spring, char gutter, char imminent)
+{
+	double var_1  = -774.9180133783279;
+	int var_2  = -96;
+	char var_3  = 'y';
+	short var_4  = -30359;
+	float var_5  = -79.83000557376553;
+	
+	erratic;
+	if (var_5 >= 35.90232973035836)
+	{
+		guide;
+		var_1;
+		if (gutter != 'S')
+		{
+			var_1;
+			var_1;
+			var_2;
+			if (var_1 < -205.6658445270025)
+			{
+				guide;
+				var_2;
+				var_5;
+				erratic;
+			}
+			else
+			{
+				var_2;
+				var_5;
+			}
+			var_2;
+			imminent;
+			var_2;
+			var_3;
+			ns_capable(var_2);
+		}
+		else
+		{
+			var_3;
+			var_1;
+			var_3;
+			if (guide == -37.91661017770625)
+			{
+				var_4;
+				gutter;
+			}
+			else
+			{
+				var_1;
+			}
+			var_1;
+			var_4;
+			imminent;
+			var_3;
+			gutter;
+			
+		}
+		erratic;
+		spring;
+		erratic;
+		engineer;
+		
+	}
+	else
+	{
+		engineer;
+		imminent;
+		var_3;
+		if (var_1 > -982.3161715477338)
+		{
+			erratic;
+			gutter;
+			gutter;
+			var_4;
+			if (imminent >= 'O')
+			{
+				guide;
+				var_4;
+				guide;
+				var_4;
+				gutter;
+			}
+			else
+			{
+				var_4;
+				guide;
+				var_5;
+			}
+			imminent;
+			spring;
+			
+		}
+		else
+		{
+			var_2;
+			var_4;
+			var_1;
+			imminent;
+			var_5;
+			if (engineer != 947.9355670126099)
+			{
+				erratic;
+				engineer;
+				gutter;
+				erratic;
+			}
+			else
+			{
+				gutter;
+				erratic;
+				var_3;
+				gutter;
+			}
+			imminent;
+			
+		}
+		spring;
+		
+	}
+	gutter;
+	gutter;
+	var_4;
+	imminent;
+	var_5;
+	
+	
+	
+	return var_2;
+}
